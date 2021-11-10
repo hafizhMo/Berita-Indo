b3584fed71173203c7ff62d3123ee470bf144445
hafizhMo <hafizh.hizb@gmail.com>
2021-11-10 17:35:06 +0700

    create comment-guide 2

--- Berita-Indo-Java/app/src/main/java/com/hafizhmo/beritaindojava/data/News.java
+++ Berita-Indo-Java/app/src/main/java/com/hafizhmo/beritaindojava/data/News.java
@@ -2,6 +2,6 @@ package com.hafizhmo.beritaindojava.data;
 
 public class News {
 
-    //TO-D0 1: initial attribute 'title', 'puDate' and 'link' using SerializedName
+    //TO-D0 1: initial attribute 'title', 'puDate', 'thumb' and 'link' using SerializedName
 
 }
